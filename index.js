ea95172a19e45339ff05184fdbc9376e100b41a1
luthfior <luthfiraama@gmail.com>
2025-05-04 06:02:42 +0700

    update index.js port

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ app.post('/auth/google', async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
